231f30fbeee6076eaf1bf4e955118dfa1998c72a
Hoang Phuong Nguyen <mldunique@gmail.com>
2026-09-06 18:26:38 +0700

    [HP] Merge comment text, fix bug single product, scrollbar 20260906

--- src/components/ApproverDetailWrapper.tsx
+++ src/components/ApproverDetailWrapper.tsx
@@ -201,7 +201,7 @@ export const ApproverDetailWrapper: React.FC<ApproverDetailWrapperProps> = ({
               <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.2" strokeLinecap="round" strokeLinejoin="round" style={{ transform: 'scaleX(-1)' }}>
                 <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
               </svg>
-              <span>Bình luận</span>
+              <span>Bình luận phản hồi</span>
             </h3>
 
             <div className="comments-list">
